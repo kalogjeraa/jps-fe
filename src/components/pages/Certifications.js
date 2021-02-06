58ea21a69c5af23056e294fd0527bdc746b2f29f
kalogjeraa <kalogjeraa@gmail.com>
2021-02-06 12:17:17 -0500

    Add Certifications.

--- src/components/pages/Certifications.js
+++ src/components/pages/Certifications.js
@@ -1,9 +1,62 @@
 import React from 'react';
 
+import Row from '../common/utilities/Row';
+import Button from '../common/utilities/Button';
+
+import ASI from '../../images/certifications/asi.png';
+import FDA from '../../images/certifications/fda-registered-facility.png';
+
+import { ASI as asi, FDA as fda } from '../../constants/certifications.js';
+
+import '../../styles/certifications.css';
+
 const Certifications = () => {
   return (
-    <div>
-      Certifications
+    <div className="container">
+      <div className="text-center row-space page-headings">
+        JETPACK SHIPPING CERTIFICATIONS
+      </div>
+      <Row special="row-space">
+        <div className="col-md-4">
+          <img
+            src={ASI}
+            alt="asi"
+            className="certifications-image-asi"
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="text-center small-headings">
+            ASI CGMP CERTIFICATION
+          </div>
+          <div className="paragraphs-text">
+            {asi}
+          </div>
+          <Button
+            styles="btn btn-primary btn-lg border-0 float-right jps-theme button-corners"
+          >
+            <span className="font-weight-bold">
+              View Certificate
+            </span>
+          </Button>
+        </div>
+      </Row>
+      <Row special="row-space">
+        <div className="col-md-4">
+          <img
+              src={FDA}
+              alt="fda"
+              className="certifications-image-fda"
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="text-center small-headings">
+            FDA REGISTERED FACILITY
+          </div>
+          <div className="paragraphs-text">
+            {fda}
+          </div>
+        </div>
+      </Row>
     </div>
   );
 };
---
+++ src/constants/certifications.js
@@ -0,0 +1,3 @@
+export const ASI = 'CGMP refers to the Current Good Manufacturing Practice regulations enforced by the FDA. CGMPs provide for systems that assure proper design, monitoring, and control of manufacturing processes and facilities. Having a CGMP facility assures the identity, strength, quality, and purity of the products we produce.';
+
+export const FDA = 'FDA regulates both finished dietary supplement products and dietary ingredients. Because dietary supplements are foods, manufacturers, packers and storage facilities are subject to FDA Food Facility Registration regulations.';
